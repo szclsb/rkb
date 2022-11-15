bcc44fc29448611bd5307f0d1ec342d27d242a02
Claudio Seitz <szclsb@gmail.com>
2022-11-15 12:11:20 +0100

    Impl first fx ui

--- app/src/main/java/ch/szclsb/rkb/app/Main.java
+++ app/src/main/java/ch/szclsb/rkb/app/Main.java
@@ -1,7 +1,35 @@
 package ch.szclsb.rkb.app;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Optional;
+
+public class Main extends Application {
+    private URL find(String resource) throws IOException {
+        return Optional.ofNullable(getClass().getResource(resource))
+                .orElseThrow(() -> new FileNotFoundException(resource));
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        var root = (Parent) FXMLLoader.load(find("main.fxml"));
+
+        var scene = new Scene(root);
+//        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+
+        primaryStage.setTitle("Remote Keyboard");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("hello world");
+        launch(args);
     }
 }
--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -1,4 +1,10 @@
 module rkb.app.main {
+    requires javafx.controls;
+    requires javafx.fxml;
+
     requires rkb.driver.main;
     requires rkb.connection.main;
+
+    opens ch.szclsb.rkb.app to javafx.fxml, javafx.graphics;
+    exports ch.szclsb.rkb.app to javafx.fxml;
 }
