5c471a5517fdd9d8a89a1833757575f395d33d3e
Claudio Seitz <szclsb@gmail.com>
2022-11-15 12:18:22 +0100

    added toggle group

--- app/src/main/java/ch/szclsb/rkb/app/FxController.java
+++ app/src/main/java/ch/szclsb/rkb/app/FxController.java
@@ -2,6 +2,7 @@ package ch.szclsb.rkb.app;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 
 public class FxController {
@@ -13,9 +14,15 @@ public class FxController {
     private TextField remoteAddressInput;
     @FXML
     private TextField remotePortInput;
+    @FXML
+    private RadioButton sendMode;
+    @FXML
+    private RadioButton receiveMode;
 
     public void initialize() {
         remoteAddressLabel.setText("remote address");
         remotePortLabel.setText("remote port");
+        sendMode.setText("send");
+        receiveMode.setText("receive");
     }
 }
